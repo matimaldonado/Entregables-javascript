433de69d178e444e593316f92c9eea1227bc4e51
Matias Maldonado <maldonadotempestti@gmail.com>
2022-04-14 00:21:04 -0300

    Modificacion completa para proyecto

--- app.js
+++ app.js
@@ -1,247 +1,253 @@
-let nombre = prompt("Ingrese su nombre y apellido completo");
-const nombr = [];
-nombr.push(nombre) ;
-alert("Hola " + nombre);
-console.log(nombr);
+//Constructor-objeto y Array
+
+let info_clientes = new Array();
+const guardo_info = "info_clientes";
+
+class Clientes {
+
+constructor(nombre,apellido,edad,fecha,mail,telefono,marca){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.fecha = fecha;
+    this.mail = mail;
+    this.telefono = telefono;
+    this.marca = marca;
+}
+}
 
 
-let dni = prompt(nombre + " ingrese su DNI");
-const documento = [];
-documento.push(dni) ;
-console.log(documento);
+//Boton y funcion
 
-let telefono = prompt(nombre + " ingrese su n° de telefono");
-const tel = [];
-tel.push(telefono) ;
-console.log(tel);
+let boton_1 = document.getElementById("btn")
 
-let fechaDeNacimiento = prompt(nombre + " ingrese su fecha de nacimiento");
-const cumpleaños = [];
-cumpleaños.push(fechaDeNacimiento) ;
-console.log(cumpleaños);
+boton_1.addEventListener("click",enviar) 
 
-let edad =Number(prompt(nombre + " Ingrese su edad"));
-const anios = [];
-anios.push(edad) ;
+function enviar(){
+    if(validarDatos()){
 
-if (anios >= 18 ) {
+        cargaDatosClientes()
+    
+    } else{
 
-    alert(nombre + " usted cumple los requisitos iniciales ¿Desea continuar?")
-    console.log (anios);
+        alert("Por favor complete todos los datos solicitados")
+    }
+}
 
-    let salario = Number(prompt(nombre + " ingrese su salario neto"))
-    const sueldo = [];
-    sueldo.push(salario) ;
-    console.log(sueldo)
+
+//Valida datos
+
+function validarDatos(){
+
+    let datos_nombre = document.getElementById("nombre").value;
+    let datos_apellido = document.getElementById("apellido").value;
+    let datos_edad = document.getElementById("edad").value;
+    let datos_fecha = document.getElementById("fecha").value;
+    let datos_mail = document.getElementById("mail").value;
+    let datos_telefono = document.getElementById("telefono").value;
+    let datos_marca = document.getElementById("marca").value;
+
+    if (!datos_nombre ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS");
+        
+        return false;
+    }
+
+    if (!datos_apellido ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+
+    if (!datos_edad ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+    if (!datos_fecha ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+    if (!datos_mail ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+    if (!datos_telefono ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+    if (!datos_marca ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS")
+        
+        return false
+    }
+
+    return true
+}
+
+//Carga datos clientes
+
+function cargaDatosClientes(){
+
+
+let nombre = document.getElementById("nombre").value
+let apellido = document.getElementById("apellido").value
+let edad = document.getElementById("edad").value
+let fecha = document.getElementById("fecha").value
+let mail = document.getElementById("mail").value
+let telefono = document.getElementById("telefono").value
+let marca = document.getElementById("marca").value
+
+let cliente = new Clientes(nombre,apellido,edad,fecha,mail,telefono,marca);
+
+validarEdad()
+
+info_clientes.push(cliente);
+
+localStorage.setItem(guardo_info,JSON.stringify(info_clientes));
+
+
+console.log(info_clientes)
+
+
+}
+
+
+//Validacion de edad
+
+function validarEdad() {
+
+    let valido_edad = document.getElementById("edad").value;
+    
+    if (valido_edad >= 18) {
+
+        nuevoFormulario()
+        
+    }else{
+
+        let h2 = document.createElement("h2")
+        h2.innerHTML = "<h2>Es necesario ser mayor de 18 años para solicitar el prestamo</h2>"
+        document.body.append(h2)
+    }
+
+    
+}
+
+//Creacion de formulario en HTML
+
+function nuevoFormulario(){
+    let div1 = document.createElement("div")
+    div1.id = "formulario2"
+    document.body.append(div1)
+
+    generarNuevoFormulario()
+}
+
+
+//Formulario 2 para funcion nuevoFormulario
+
+function generarNuevoFormulario(){
+
+      let p1 = document.createElement("h2")
+    p1.innerHTML = "<h2>Sus datos está correctos, por favor ingrese los datos restantes para calcular monto disponible y cuota</h2>"
+    formulario2.append(p1)
+    
+    let input1 = document.createElement("input")
+    input1.type = "Number"
+    input1.id = "salario"
+    input1.required
+    input1.placeholder = "Ingrese su salario neto"
+    formulario2.append(input1)
+
+    let boton_2 = document.createElement("button")
+    boton_2.type = "button"
+    boton_2.id = "btn2"
+    boton_2.innerHTML = "Enviar"
+    formulario2.append(boton_2)
+}
+
+
+//Verificacion de Salario y montos para cuotas
+
+function ingreso_salario() {
+
+    let salario = document.getElementById("salario").value;
+    info_clientes.push(salario)
+
+    console.log(info_clientes)
 
     if (salario >= 50001 && salario <= 100000 ) { 
     
-        alert(nombre + " usted puede acceder a una finaciacion de $200000")
+        let p2 = document.createElement("p")
+        p2.innerHTML = "<p> Usted puede acceder a una finaciacion de $200000, en breve un asesor se contactará </p>"
+        document.body.append(p2)
         
-        function interes(n1 , n2 , impuesto ) {
-            
-            return (n1 / n2) * impuesto;}
-            
-            let valor1 = 200000;
-            let valor2 = Number(prompt("Ingrese cantidad de cuotas entre 1 y 60"));
-            let valor3 = 1.35;
-            let valor4 = interes(valor1, valor2, valor3);
-
-            console.log (valor2)
-
-            alert("Su cuota será de $" + valor4)
-            console.log (valor4);
-        
-
-        let eleccionVehiculo = prompt("Elegí la marca del vehículo entre estas opciones:FORD,CHEVROLET,FIAT,PEUGEOT").toUpperCase();
-        const auto = [];
-        auto.push(eleccionVehiculo.toUpperCase()) ;
-        console.log(auto);
-        
-
-        switch (eleccionVehiculo) {
-        
-            case "FORD" :
-                alert("Usted elegió la marca Ford");
-                break;            
-            
-            case "FIAT":
-                alert("Usted elegió la marca Fiat");
-                break;
-        
-            case "CHEVROLET":
-                alert("Usted elegió la marca Chevrolet");
-                break;
-            case "PEUGEOT":
-                alert("Usted elegió la marca Peugeot");
-                break;
-        
-            default:
-                alert("El dato ingresado no corresponde, vuelva a cargar la página")
-               break;
-        } 
-        
-    
     }else if (salario >= 100001 && salario <= 150000 ) {
-        alert(nombre + " usted puede acceder a una finaciacion de $400000")
-        
-        function interes(n3 , n4 , impuest ) {
-            
-            return (n3 / n4) * impuest;}
-            
-            let valor5 = 400000;
-            let valor6 = Number(prompt("Ingrese cantidad de cuotas entre 1 y 60"));
-            let valor7 = 1.35;
-            let valor8 = interes(valor5, valor6, valor7);
-
-            console.log (valor6)
-
-            alert("Su cuota será de $" + valor8)
-            console.log (valor8)
-
-
-        let eleccionVehiculo = prompt("Elegí la marca del vehículo entre estas opciones:FORD,CHEVROLET,FIAT,PEUGEOT").toUpperCase();
-        const auto = [];
-        auto.push(eleccionVehiculo.toUpperCase()) ;
-        console.log(auto);
-        
-
-        switch (eleccionVehiculo) {
-        
-            case "FORD" :
-                alert("Usted elegió la marca Ford");
-                break;            
-            
-            case "FIAT":
-                alert("Usted elegió la marca Fiat");
-                break;
-        
-            case "CHEVROLET":
-                alert("Usted elegió la marca Chevrolet");
-                break;
-            case "PEUGEOT":
-                alert("Usted elegió la marca Peugeot");
-                break;
-        
-            default:
-                alert("El dato ingresado no corresponde, vuelva a cargar la página")
-               break;
-        } 
+        let p3 = document.createElement("p")
+        p3.innerHTML = "<p> Usted puede acceder a una finaciacion de $400000, en breve un asesor se contactará </p>"
+        document.body.append(p3)
         
     }else if (salario >= 150001 && salario <= 200000 ) {
-        alert(nombre + " usted puede acceder a una finaciacion de $600000")
-
-        function interes(n5 , n6 , impues ) {
-            
-            return (n5 / n6) * impues;}
-            
-            let valor9 = 600000;
-            let valor10 = Number(prompt("Ingrese cantidad de cuotas entre 1 y 60"));
-            let valor11 = 1.35;
-            let valor12 = interes(valor9, valor10, valor11);
-
-            console.log (valor10)
-
-            alert("Su cuota será de $" + valor12)
-            console.log (valor12)
-
-        let eleccionVehiculo = prompt("Elegí la marca del vehículo entre estas opciones:FORD,CHEVROLET,FIAT,PEUGEOT").toUpperCase();
-        const auto = [];
-        auto.push(eleccionVehiculo.toUpperCase()) ;
-        console.log(auto);
-        
-
-        switch (eleccionVehiculo) {
-        
-            case "FORD" :
-                alert("Usted elegió la marca Ford");
-                break;            
-            
-            case "FIAT":
-                alert("Usted elegió la marca Fiat");
-                break;
-        
-            case "CHEVROLET":
-                alert("Usted elegió la marca Chevrolet");
-                break;
-            case "PEUGEOT":
-                alert("Usted elegió la marca Peugeot");
-                break;
-        
-            default:
-                alert("El dato ingresado no corresponde, vuelva a cargar la página")
-               break;
-        } 
-
+        let p4 = document.createElement("p")
+        p4.innerHTML = "<p> Usted puede acceder a una finaciacion de $600000, en breve un asesor se contactará </p>"
+        document.body.append(p4)
+    
     }else if (salario >= 200001) {
-        alert(nombre + " usted puede acceder a una finaciacion de $1000000")
-        function interes(n7 , n8 , imp ) {
-            
-            return (n7 / n8) * imp;}
-            
-            let valor13 = 1000000;
-            let valor14 = Number(prompt("Ingrese cantidad de cuotas entre 1 y 60"));
-            let valor15 = 1.35;
-            let valor16 = interes(valor13, valor14, valor15);
+        let p5 = document.createElement("p")
+        p5.innerHTML = "<p> Usted puede acceder a una finaciacion de $1000000, en breve un asesor se contactará </p>"
+        document.body.append(p5)
+    }else{
+        let p6 = document.createElement("p")
+        p6.innerHTML = "<p> Su salario no cumple los requisitos necesarios, en breve un asesor se contactará </p>"
+        document.body.append(p6)
 
-            console.log (valor14)
+    }    
+}
 
-            alert("Su cuota será de $" + valor16)
-            console.log (valor16)
+//Funcion boton 2
 
-        let eleccionVehiculo = prompt("Elegí la marca del vehículo entre estas opciones:FORD,CHEVROLET,FIAT,PEUGEOT").toUpperCase();
-        const auto = [];
-        auto.push(eleccionVehiculo.toUpperCase()) ;
-        console.log(auto);
+let boton_2 = document.getElementById("btn2");
+
+boton_2.onclick = enviar2
+
+
+
+function enviar2(){
+ 
+    if(validarDatos2){
+  
+        ingreso_salario()
+
+ }else{
+   alert("Complete los datos solicitados")
+ }   
+}
+
+//Validacion salario
+function validarDatos2(){
+
+    let datos_salario = document.getElementById("salario").value;
+    
+    if (!datos_salario ){
+        alert("INGRESE TODOS LOS DATOS SOLICITADOS");
         
+        return false;
+    }
 
-        switch (eleccionVehiculo) {
-        
-            case "FORD" :
-                alert("Usted elegió la marca Ford");
-                break;            
-            
-            case "FIAT":
-                alert("Usted elegió la marca Fiat");
-                break;
-        
-            case "CHEVROLET":
-                alert("Usted elegió la marca Chevrolet");
-                break;
-            case "PEUGEOT":
-                alert("Usted elegió la marca Peugeot");
-                break;
-        
-            default:
-                alert("El dato ingresado no corresponde, vuelva a cargar la página")
-               break;
-        } 
+    return true;
+}
 
-    } else{
-        alert(nombre + "su salario no cumple con el minimo necesario.")}
-           
-} else{ alert (nombre + " usted no cumple los requisitos necesarios") ;
-console.log (anios);}
 
-const datosUsuarios = nombr.concat(documento,tel,cumpleaños,anios)
+function recibir_info_clientes(){
 
-console.log(datosUsuarios)
+    let recibo = localStorage.getItem(guardo_info);
+    if (recibo){
 
-console.dir(document)
+        recibo = JSON.parse(recibo);
 
-let txt = document.getElementsByTagName("p")
-console.log(txt)
-
-let agradece = document.getElementById("gracias")
-agradece.innerText = "Muchas gracias en breve uno de nuestros asesores se pondrá en contacto con usted para completar su trámite"
-
-let agrego = document.getElementById("contact")
-agrego.innerHTML = "<h2>Contactanos</h2>"
-console.log(agrego)
-
-let crear = document.createElement("p")
-crear.innerHTML = "<p>Tel:2615698167</p> <p>Domicilio:Calle Falsa 123</p>"
-document.body.append(crear)
-
-let borrar = document.getElementById("subtitulo")
-borrar.remove();
+        guardo_info = recibo;
+    }
+}
